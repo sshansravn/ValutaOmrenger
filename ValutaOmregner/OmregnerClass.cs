d0df64f618f9f5b7b18dd55f7a72a789ea6ae1c6
Hans Ravnsfjall <hra@sendistovan.fo>
2020-03-09 22:19:27 +0000

    Initial check-in of module ValutaOmregner

---
+++ ValutaOmregner/OmregnerClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ValutaOmregner
+{
+    public class omregnerClass{
+       public float dkk;
+       public const float kurs= 7.45f;
+       
+        public void calculateCurrency() {
+
+            Console.WriteLine(dkk + " danske kroner, svarer til " + (dkk/100)*kurs + " euro");
+        }
+
+    }
+}
---
+++ ValutaOmregner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValutaOmregner
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var calctest = new omregnerClass();
+            Console.WriteLine("Indtast beløb i danske kroner");
+            Console.WriteLine("");
+            string tempCurr = Console.ReadLine();
+            calctest.dkk = (float) Convert.ToDouble(tempCurr);
+            calctest.calculateCurrency();
+
+
+        }
+    }
+}
